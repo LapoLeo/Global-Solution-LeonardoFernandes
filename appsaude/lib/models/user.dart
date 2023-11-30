a32a47211d15645a9bb2c9de20a8f199aaaf4433
LeoLapo <leonardo.lapo.fernandes@gmail.com>
2023-11-29 21:58:39 -0300

    Revert "aa"
    This reverts commit e3c94aa5c5f04d1a414dbd774f4c0073c8f73e4a.

--- appsaude/lib/models/user.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class User with ChangeNotifier {
-  final String id;
-  final String name;
-  final String email;
-
-  User(this.id, this.name, this.email);
-}
--- appsaude/lib/sign-up.dart
+++
@@ -1,5 +0,0 @@
-// lib/screens/signup_page.dart
-import 'package:flutter/material.dart';
-import 'user.dart';
-
-// ... (Add the SignUpPage and related classes here)
